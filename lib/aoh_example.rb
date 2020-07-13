376c93adf6c1be1d0462931746772df9ba9a0418
Juan Carlos Patron <charliepatronr@gmail.com>
2020-07-13 23:35:33 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -1,7 +1,7 @@
 # Examples inspired by David Foster Wallace's # (2/21/1962 to 9/12/2008) _Infinite Jest_
 
 DON_G = { name:  "Don Gately", occupation: "Live-in Staff" }
-JOELLE_VD = { name:  "Don Gately", occupation: "Live-in Staff" }
+JOELLE_VD = { name:  "Joelle van Dyne", occupation: "Radio Personality" }
 PAT_M =  { name:  "Pat Monteseian", occupation: "Staff" }
 KATE_G = { name:  "Kate Gompert", occupation: "None" }
 BRUCE_G = { name:  "Bruce Green", occupation: "Fan of Mildred" }
@@ -14,12 +14,9 @@ def literal_aoh
   # Using Array literal syntax only, build a nested array that uses the data in
   # held in the constants
   
-  arr = [{ name:  "Don Gately", occupation: "Live-in Staff" },
-        { name:  "Don Gately", occupation: "Live-in Staff" }, 
-        { name:  "Pat Monteseian", occupation: "Staff" }, 
-        { name:  "Kate Gompert", occupation: "None" }, 
-        { name:  "Bruce Green", occupation: "Fan of Mildred" }
-
+    arr = [{ name:  "Don Gately", occupation: "Live-in Staff" },
+  { name:  "Joelle van Dyne", occupation: "Radio Personality" }
+  
         ]
 end
 
